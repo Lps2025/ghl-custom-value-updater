58c214cb8a730fa34a0dda680831388851ad6b15
Lps2025 <carson.krystina77@gmail.com>
2025-04-17 13:21:06 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 // pages/index.js
 
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      if (event.data?.locationId) {
+        const hiddenInput = document.querySelector('input[name="location_id"]');
+        if (hiddenInput) {
+          hiddenInput.value = event.data.locationId;
+        }
+      }
+    });
+
+    // Ask parent (GHL) for location info
+    window.opener?.postMessage({ action: "getAppInfo" }, "*");
+  }, []);
+
   return (
     <div style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>LaunchPoint Setup Form</h1>
@@ -15,11 +31,10 @@ export default function Home() {
         <label>About Section</label><br />
         <textarea name="about_text"></textarea><br /><br />
 
-        <input type="hidden" name="location_id" value="REPLACE_WITH_LOCATION_ID" />
+        <input type="hidden" name="location_id" value="" />
 
         <button type="submit">Submit</button>
       </form>
     </div>
   );
 }
-
