b1108165f93eb5dd873c192e80fb65b8e38bf1a2
Lps2025 <carson.krystina77@gmail.com>
2025-04-17 14:07:19 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL", // Allow GHL to iframe this app
+          },
+        ],
+      },
+    ];
+  },
+};
